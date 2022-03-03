e00a983636cf3227dac98128829d0bc4cadb2f45
adaj-1 <67992220+adaj-1@users.noreply.github.com>
2022-03-03 14:48:49 -0700

    working single producer/consumer non-vegan

--- Assign2/Assign2.cpp
+++ Assign2/Assign2.cpp
@@ -6,10 +6,8 @@
 #include <time.h>      //time
 
 #define MAX_BUFFER_SIZE 10 // tray size
-#define FCA 1              // Fettuccine Chicken Alfredo Non-Vegan Buffer
-#define GSS 2              // Garlic Sirloin Steak Non-Vegan Buffer
-#define PPP 1              // Pistachio Pesto Pasta Vegan Buffer
-#define AFS 2              // Avocado Fruit Salad Vegan Buffer
+#define dish1 1            // Fettuccine Chicken Alfredo Non-Vegan Buffer or Pistachio Pesto Pasta Vegan Buffer
+#define dish2 2            // Garlic Sirloin Steak Non-Vegan Buffer or Avocado Fruit Salad Vegan Buffer
 
 int main()
 {
@@ -17,6 +15,8 @@ int main()
     int *nv_tray = (int *)mmap(NULL, MAX_BUFFER_SIZE * sizeof(int *), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     int *nv_tray_in = (int *)mmap(NULL, MAX_BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);  // initialize to index 0
     int *nv_tray_out = (int *)mmap(NULL, MAX_BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0); // initalize to index 0
+    *nv_tray_in = 0;
+    *nv_tray_out = 0;
     // Create shared semaphore for non-vegan tray
     // This is going to act as a binary sempahore
     sem_t *nv_mutex = (sem_t *)mmap(NULL, MAX_BUFFER_SIZE * sizeof(sem_t *), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -32,6 +32,8 @@ int main()
     int *v_tray = (int *)mmap(NULL, MAX_BUFFER_SIZE * sizeof(int *), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     int *v_tray_in = (int *)mmap(NULL, MAX_BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);  // initialize to index 0
     int *v_tray_out = (int *)mmap(NULL, MAX_BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0); // initalize to index 0
+    *v_tray_in = 0;
+    *v_tray_out = 0;
     // Create shared semaphore for vegan tray
     // This is going to act as a binary sempahore
     sem_t *v_mutex = (sem_t *)mmap(NULL, MAX_BUFFER_SIZE * sizeof(sem_t *), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -55,12 +57,27 @@ int main()
     {
         while (1)
         {
+            // next produced item
+            srand(time(0));
+            int next_produced = 1 + rand() % 2;
+            printf("next_produced: %d ", next_produced);
+
             sem_wait(nv_empty); // empty is the amount of empty slots in the semiphore, wait until empty is not 0
             sem_wait(nv_mutex); // lock
 
             // CRITICAL SECTION, (ADD TO BUFFER)
-            printf("Producer creates something\n");
-            // left us to implement in and out by ourselves UDPATE IN AND OUT
+            if (next_produced == 1)
+            {
+                nv_tray[*nv_tray_in] = dish1;
+                printf("Donatello creates non-vegan dish: Fettuccine Chicken Alfredo\n");
+            }
+            else if (next_produced == 2)
+            {
+                nv_tray[*nv_tray_in] = dish2;
+                printf("Donatello creates non-vegan dish: Garlic Sirloin Steak\n");
+            }
+
+            *nv_tray_in = (*nv_tray_in + 1) % MAX_BUFFER_SIZE;
 
             sem_post(nv_mutex); // unlock
             sem_post(nv_full);
@@ -76,9 +93,22 @@ int main()
             sem_wait(nv_full);
             sem_wait(nv_mutex); // lock
 
+            srand(time(0));
+            // next consumed item
+            int next_consumed = nv_tray[*nv_tray_out];
+            printf("next_consumed: %d ", next_consumed);
+
             // CRITICAL SECTION (TAKE FROM BUFFER)
-            printf("Consumer takes something\n");
-            // left us to implement in and out by ourselves UDPATE IN AND OUT
+            if (next_consumed == 1)
+            {
+                printf("Non-vegan customer removes non-vegan dish: Fettuccine Chicken Alfredo\n");
+            }
+            else if (next_consumed == 2)
+            {
+                printf("Non-vegan customer removes non-vegan dish: Garlic Sirloin Steak\n");
+            }
+
+            *nv_tray_out = (*nv_tray_out + 1) % MAX_BUFFER_SIZE;
 
             sem_post(nv_mutex); // unlock
             sem_post(nv_empty);
@@ -93,7 +123,7 @@ int main()
         {
             int takenSlots;
             sem_getvalue(nv_full, &takenSlots);
-            printf("Items in the buffer %d/%d\n", takenSlots, MAX_BUFFER_SIZE);
+            printf("Items in the non-vegan tray %d/%d\n", takenSlots, MAX_BUFFER_SIZE);
             sleep(10); // TODO check if this is the correct sleep time
         }
     }
